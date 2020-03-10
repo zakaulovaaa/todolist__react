31166fa24fcc8517ce07fbc6c8222a5d8663418b
zakaulovaaa <darya.zakaulova@yandex.ru>
2020-03-10 21:30:08 +0800

    Updates

---
+++ precache-manifest.3d9231f42726bb7dc37a3a5a7b518dd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cd8cc6a5a3c7449a210063d55b2f02",
+    "url": "/opapa/index.html"
+  },
+  {
+    "revision": "8e876c06071d1e0aee3b",
+    "url": "/opapa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5dd0e484eff436ca841a",
+    "url": "/opapa/static/js/2.217aeb40.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/opapa/static/js/2.217aeb40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e876c06071d1e0aee3b",
+    "url": "/opapa/static/js/main.9a6e1218.chunk.js"
+  },
+  {
+    "revision": "67b14df3a29d217c58cb",
+    "url": "/opapa/static/js/runtime-main.ff1993eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/opapa/static/media/logo.5d5d9eef.svg"
+  }
+]);
