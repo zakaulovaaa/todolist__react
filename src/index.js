1e08dc63db0d1d841860bc4857b1261fc71d440d
zakaulovaaa <darya.zakaulova@yandex.ru>
2020-03-11 10:39:31 +0800

    one

--- src/index.js
+++ src/index.js
@@ -13,21 +13,11 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-// import Open_my_popap from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
 
 let elem = document.getElementById('add_task');
 elem.addEventListener("click", open_popap);
 
 function open_popap() {
-
     const element = <Open_my_popap
         popap_openis={true}
         task_new={true}
